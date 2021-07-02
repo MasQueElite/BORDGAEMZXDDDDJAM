b1301369ef62ac2651da75b04210fd9f1d2b5859
MasQueElite <68552309+MasQueElite@users.noreply.github.com>
2021-07-02 12:05:03 +0200

    Fix the effing bugs
    ffs Unity...

--- jam/Assets/BORDGAEMXD.cs
+++ jam/Assets/BORDGAEMXD.cs
@@ -16,141 +16,114 @@ public class BORDGAEMXD : MonoBehaviour
 	public KMSelectable[] yourHoles;
 	public Mesh[] seedsPos;
 
-	private int[] seeds = new int[16];
+	private int[] seeds = Enumerable.Repeat(7, 16).ToArray();
 	private int[] initialState = new int[16];
-	//private int limit = 10;
-	private int totalNumSeeds = 98;
+
 	private int[] mostSeeds = new int[7];
 
-	private bool myTurn = true;
+	private bool turn = true;
+	private int intedBool;
 
 	static int moduleIdCounter = 1;
 	int moduleId;
 	private bool moduleSolved;
 
-	void Awake() {
+	void Awake ()
+	{
 		moduleId = moduleIdCounter++;
 	}
 
 	// Use this for initialization
-	void Start() {
-		distribute(totalNumSeeds);
-		Array.Copy(getTheMost(), mostSeeds, 7);
+	void Start ()
+	{
+		distribute();
+		Array.Copy(getTheMost(), mostSeeds, mostSeeds.Length);
 		Debug.Log("Seeds: " +
 					mostSeeds.Max() +
-			      "in position: " +
-				    Array.IndexOf(mostSeeds, mostSeeds.Max()) +
+				  "in position: " +
+					Array.IndexOf(mostSeeds, mostSeeds.Max()) +
 				  ".");
 	}
 
 	//Update is called once per frame
-	void Update()
+	void Update ()
 	{
 
 	}
 
-	void distribute(int tns)
+	void distribute ()
 	{
-		/*/for (int i = 0; i < seeds.Length; i++)
-		{
-			seeds[i] = UnityEngine.Random.Range(0, tns + 1);
-			tns -= seeds[i];
-		}
-		seeds.Shuffle();/*/
-		/*/for (int lmao, lmao < seeds.Length; lmao++)//lmao
-		  initialState[lmao] = seeds[lmao];/*/
-		for (int i = 0; i < seeds.Length; i++) { seeds[i] = 7; }
 		seeds[7] = 0;
 		seeds[15] = 0;
-		int turns = UnityEngine.Random.Range(8, 15);
+		int turns = UnityEngine.Random.Range(8, 15 + 1);
 		for (int j = 0; j <= turns; j++)
 		{
-			if (myTurn == true)
-			{
-				int rng = UnityEngine.Random.Range(0, 7);
-				playAturn(rng);
-				myTurn = false;
-			}
-			else
-			{
-				int rng = UnityEngine.Random.Range(8, 15);
-				playAturn(rng);
-				myTurn = true;
-			}
+			int rng = UnityEngine.Random.Range(getBoundsFromPlayer(turn, false), getBoundsFromPlayer(turn, true));
+			playAturn(rng, turn);
+			turn = !turn;
 		}
-		Array.Copy(seeds, initialState, 7);
+		Array.Copy(seeds, initialState, seeds.Length);
 		for (int k = 0; k < seeds.Length; k++)
-        {
 			holes[k].text = seeds[k].ToString();
-        }
-
 	}
 
-	int[] getTheMost() {
+	int int2bool (bool originalBool)
+	{
+		return originalBool ? 1 : 0;
+	}
+
+	int getBoundsFromPlayer (bool player, bool upperBound)
+	{ //P0 is the human; P1 is the machine; P = player
+		if (upperBound) return int2bool(player) * 8 + 7; // = player's store/main hole
+		else return int2bool(player) * 8;
+	}
+
+	int[] getTheMost ()
+	{
 		int[] result = new int[7];
 
 		for (int holeNumber = 0; holeNumber < 7; holeNumber++)
 		{
-			playAturn(holeNumber);
+			playAturn(holeNumber, false);
 			getTotalPts(holeNumber, result);
 			reset();
 		}
 		return result;
 	}
 
-	void playAturn(int hn) {
-		if (myTurn == true)
-        {
-			while (!(hn == 7 || seeds[hn] == 1 || seeds[hn] == 0))
-			{// When last seed lands on player's store or in an empty hole, the last condition is for the if part
-				int hold = seeds[hn];
-				seeds[hn] = 0;
-				for (; hold > 0; hn++, hold--)
-				{//Distrubuting seeds xdxdxd what. <- the first argument is useless bc we already have declared the variable hold
-					if (hn == 15) hn = 0; //Skipping opponent's store/HoleNumber
-					seeds[hn]++;
-				}
-				if (seeds[hn] == 1 && hn >= 0 && hn <= 6)
-				{// Accounting for when last seed lands on own empty hole
-					seeds[7] += seeds[hn] + seeds[14 - hn];
-					seeds[hn] = 0;
-					seeds[14 - hn] = 0;
-				}
-			}
-		}
-        else
-        {
-			while (!(hn == 15 || seeds[hn] == 1 || seeds[hn] == 0))
-			{// When last seed lands on player's store or in an empty hole, the last condition is for the if part
-				int hold = seeds[hn];
-				seeds[hn] = 0;
-				for (; hold > 0; hn++, hold--)
-				{//Distrubuting seeds xdxdxd what. <- the first argument is useless bc we already have declared the variable hold
-					if (hn == 7) hn = 8; //Skipping opponent's store/HoleNumber
-					if (hn >= 16) hn = 0;
-					seeds[hn]++;
-				}
-				if (seeds[hn] == 1 && hn >= 8 && hn <= 14)
-				{// Accounting for when last seed lands on own empty hole
-					seeds[15] += seeds[hn] + seeds[14 - hn];
-					seeds[hn] = 0;
-					seeds[14 - hn] = 0;
-				}
-			}
-		}
-		
-	}
-
-	void getTotalPts(int hn, int[] result){
-		for (int h = 0; h < 7; h++)
-			result[hn] += seeds[h];
-	}
-
-	void reset()
+	void playAturn (int hn, bool turn)
 	{
-		/*/
-		for (int j; j < seeds.Length; j++) //Resets the board
-		  seeds[j] = initialState[j];/*/
+		int[] bounds = { getBoundsFromPlayer(turn, false), getBoundsFromPlayer(turn, true), getBoundsFromPlayer(!turn, true) };
+			// = {lower bound from current player, upper bound from current player, upper bound from the opponent} --- I don't need the lower bound from the opponent
+
+		while (!(hn == bounds[1] || seeds[hn] == 1 || seeds[hn] == 0))
+		{// When last seed lands on player's store or in an empty hole, the last condition is for the if part
+			int hold = seeds[hn];
+			seeds[hn] = 0;
+			for (; hold > 0; hold--)
+			{ //Distrubuting seeds
+				hn++;
+				if (hn == bounds[2]) hn = bounds[0]; //Skipping opponent's store/HoleNumber
+				else if (hn >= 16) hn = 0;
+				seeds[hn]++;
+			}
+			if (seeds[hn] == 1 && hn >= bounds[0] && hn < bounds[1])
+			{// Accounting for when last seed lands on own empty hole
+				seeds[bounds[1]] += seeds[hn] + seeds[14 - hn];
+				seeds[hn] = 0;
+				seeds[14 - hn] = 0;
+			}
+		}
+	}
+
+	void getTotalPts (int hn, int[] result)
+	{
+		foreach (int hole in seeds)
+			result[hn] += hole;
+	}
+
+	void reset ()
+	{
 		Array.Copy(initialState, seeds, 7);
 	}
 }
