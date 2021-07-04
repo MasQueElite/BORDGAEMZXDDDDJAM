03e94727d48c0fa0d8684c681a4c08ddcfd49c10
ktane1 <jovanboh@hotmail.com>
2021-07-04 19:46:29 +0800

    Lots of things updated
    Bottom text

--- jam/Assets/BORDGAEMXD.cs
+++ jam/Assets/BORDGAEMXD.cs
@@ -14,7 +14,8 @@ public class BORDGAEMXD : MonoBehaviour
 
 	public TextMesh[] holes;
 	public KMSelectable[] yourHoles;
-	public Mesh[] seedsPos;
+	public GameObject[] seedsPlacement;
+	public GameObject[] pivot;
 
 	private int[] seeds = Enumerable.Repeat(7, 16).ToArray();
 	private int[] initialState = new int[16];
@@ -26,32 +27,45 @@ public class BORDGAEMXD : MonoBehaviour
 
 	static int moduleIdCounter = 1;
 	int moduleId;
-	private bool moduleSolved;
+	private bool moduleSolved = false;
 
 	void Awake ()
 	{
 		moduleId = moduleIdCounter++;
+		foreach (KMSelectable hole in yourHoles)
+        {
+			KMSelectable selectedHole = hole;
+			hole.OnInteract += delegate () { holeHandler(hole); return false; };
+        }
 	}
 
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
 		distribute();
 		Array.Copy(getTheMost(), mostSeeds, mostSeeds.Length);
 		Debug.Log("Seeds: " +
 					mostSeeds.Max() +
 				  " in hole: " +
-					(Array.IndexOf(mostSeeds, mostSeeds.Max()+1)) +
+					(Array.IndexOf(mostSeeds, mostSeeds.Max()) + 1) +
 				  ".");
 
-		foreach (int hole in initialState)
-			Debug.Log("Initial state: " + hole);
+		logArray(seeds);
 	}
 
 	//Update is called once per frame
 	void Update ()
 	{
 
+	}
+
+	void logArray(int[] arr)
+	{
+		string str = "";
+		for (int i = 0; i < arr.Length; i++)
+			str += arr[i] + " ";
+
+		Debug.Log(str);
 	}
 
 	void distribute ()
@@ -63,29 +77,36 @@ public class BORDGAEMXD : MonoBehaviour
 		turn = turns % 2 == 0 ? false : true;
 		for (int j = 0; j <= turns; j++)
 		{
-            do rng = UnityEngine.Random.Range(getBoundsFromPlayer(turn, false), getBoundsFromPlayer(turn, true));
+			do rng = UnityEngine.Random.Range(getBoundsFromPlayer(turn, false), getBoundsFromPlayer(!turn, true));
 			while (seeds[rng] <= 0);
-			
+
 			playAturn(rng, turn);
 			turn = !turn;
 		}
 		Array.Copy(seeds, initialState, seeds.Length);
-		for (int k = 0; k < seeds.Length; k++)
-			holes[k].text = seeds[k].ToString();
+		for (int i = 0; i < seedsPlacement.Length; i++)
+		{
+			for (int k = 0; k < pivot.Length; k++)
+			{
+				Vector3 newPos = (new Vector3(UnityEngine.Random.Range((pivot[k].transform.position.x - 0.003f), (pivot[k].transform.position.x + 0.003f)), 0.09f, UnityEngine.Random.Range((pivot[k].transform.position.z - 0.003f), (pivot[k].transform.position.z + 0.003f))));
+				seedsPlacement[i].transform.position = newPos;
+			}
+
+		}
 	}
 
-	int int2bool (bool originalBool)
+	int int2bool(bool originalBool)
 	{
 		return originalBool ? 1 : 0;
 	}
 
-	int getBoundsFromPlayer (bool player, bool upperBound)
+	int getBoundsFromPlayer(bool player, bool upperBound)
 	{ //P0 is the human; P1 is the machine; P = player
-		if (upperBound) return int2bool(player) * 8 + 7; // = player's store/main hole
+		if (upperBound) return int2bool(!player) * 8 + 7; // = player's store/main hole
 		else return int2bool(player) * 8;
 	}
 
-	int[] getTheMost ()
+	int[] getTheMost()
 	{
 		int[] result = new int[7];
 
@@ -93,9 +114,10 @@ public class BORDGAEMXD : MonoBehaviour
 		{
 			if (seeds[holeNumber] > 0)
 			{
+				Debug.Log("----------------- PLAYING HOLE " + (holeNumber + 1) + ":");
 				playAturn(holeNumber, false);
-				Debug.Log("Total number of seeds: " + Enumerable.Sum(seeds) + " for hole: " + (holeNumber+1)); //debug
-													//this should ALWAYS be 98
+				Debug.Log("Total number of seeds: " + Enumerable.Sum(seeds) + " for hole: " + (holeNumber + 1)); //debug
+																												 //this should ALWAYS be 98
 				getTotalPts(holeNumber, result);
 				reset();
 			}
@@ -103,10 +125,10 @@ public class BORDGAEMXD : MonoBehaviour
 		return result;
 	}
 
-	void playAturn (int hn, bool turn)
+	void playAturn(int hn, bool turn)
 	{
-		int[] bounds = { getBoundsFromPlayer(turn, false), getBoundsFromPlayer(turn, true), getBoundsFromPlayer(!turn, true) };
-			// = {lower bound from current player, upper bound from current player, upper bound from the opponent} --- I don't need the lower bound from the opponent
+		int[] bounds = { getBoundsFromPlayer(turn, false), getBoundsFromPlayer(turn, true), getBoundsFromPlayer(!turn, false), getBoundsFromPlayer(!turn, true) };
+		// = {lower bound from current player, upper bound from current player, lower bound from the opponent, upper bound from the opponent}
 
 		while (!(hn == bounds[1] || seeds[hn] == 1))
 		{// When last seed lands on player's store or in an empty hole
@@ -115,28 +137,51 @@ public class BORDGAEMXD : MonoBehaviour
 			for (; hold > 0; hold--)
 			{ //Distrubuting seeds
 				hn++;
-				if (hn == bounds[2]) hn = bounds[0]; //Skipping opponent's store/HoleNumber
+				if (hn == bounds[3]) hn = bounds[2]; //Skipping opponent's store/HoleNumber
 				else if (hn >= 16) hn = 0;
 				seeds[hn]++;
 			}
-			if (seeds[hn] == 1 && hn >= bounds[0] && hn < bounds[1])
+			Debug.Log("Ended in hole: " + (hn + 1));
+			logArray(seeds);
+			if (seeds[hn] == 1 && hn >= bounds[0] && hn < bounds[3])
 			{// Accounting for when last seed lands on own empty hole
 				seeds[bounds[1]] += seeds[hn] + seeds[14 - hn];
 				seeds[hn] = 0;
 				seeds[14 - hn] = 0;
+				Debug.Log("Capturing hole " + (14 - hn + 1));
+				logArray(seeds);
 				break;
 			}
 		}
 	}
 
-	void getTotalPts (int hn, int[] result)
+	void getTotalPts(int hn, int[] result)
 	{
 		for (int i = 0; i <= 7; i++)
 			result[hn] += seeds[i];
-		Debug.Log("Points gained if the player played this hole: " + result[hn] + " whith value: " + initialState[hn]);
+		Debug.Log("Points gained if the player played this hole: " + result[hn] + " with value: " + initialState[hn]);
 	}
 
-	void reset () {
+	void reset()
+	{
 		Array.Copy(initialState, seeds, initialState.Length);
 	}
+
+	void holeHandler(KMSelectable hole)
+    {
+		if (moduleSolved == false)
+		{
+			if (hole == yourHoles[0]) //Enter most seeds hole here
+			{
+				moduleSolved = true;
+				Debug.Log("You selected the correct hole. Module solved.");
+				Module.HandlePass();
+			}
+			else
+			{
+				Debug.Log("You selected the wrong hole. Strike.");
+				Module.HandleStrike();
+			}
+		}
+    }
 }
